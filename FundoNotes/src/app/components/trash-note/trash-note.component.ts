9a075d398bc90d4ac34087a194d03b72071a3cbd
Amit Kumar Nayak <amitkumarnayak40@gmail.com>
2023-09-22 16:47:14 +0530

    [Amit]Added: Show all Trash note and implement Child-Parent Data sharing init

--- FundoNotes/src/app/components/trash-note/trash-note.component.ts
+++ FundoNotes/src/app/components/trash-note/trash-note.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NoteService } from 'src/app/services/Note/note.service';
 
 @Component({
   selector: 'app-trash-note',
   templateUrl: './trash-note.component.html',
   styleUrls: ['./trash-note.component.scss']
 })
-export class TrashNoteComponent {
-
+export class TrashNoteComponent implements OnInit{
+  noteArray = [];
+  constructor(private note:NoteService){}
+  
+  ngOnInit(){
+    this.GetAllTrashNode();
+  }
+  GetAllTrashNode(){
+    this.note.GetAllNote().subscribe((response: any) => {
+      //console.log(response);
+      this.noteArray = response.data;
+      this.noteArray = this.noteArray.filter((result:any)=>{
+        return result.isAechive == false && result.isTrash == true;
+      })
+      console.log(this.noteArray);
+    });
+  }
+  refreshTrasnNote($event:any){
+    this.GetAllTrashNode();
+  }
 }
